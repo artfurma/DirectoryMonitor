c73cdc94b45690aa8b0f6ed583f8f322ffbaf12d
Artur Furmańczyk <furmanczyk.artur@gmail.com>
2017-12-30 17:25:31 +0100

    Configure log4net, add helper libs

---
+++ DirectoryMonitor/Libs/WatcherLib.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryMonitor.Libs
+{
+	public static class WatcherLib
+	{
+		private static readonly log4net.ILog Log =
+			log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		public static void StartMonitoring()
+		{
+			ConfigureWatcher();
+			Log.Info("Rozpoczęto monitorowanie");
+		}
+
+		public static void StopMonitoring()
+		{
+			Log.Info("Zakończono monitorowanie");
+		}
+
+		private static void ConfigureWatcher()
+		{
+			var watcher = new FileSystemWatcher
+			{
+				Path = AppSettingsHelper.GetSetting("Path"),
+				Filter = AppSettingsHelper.GetSetting("Filter")
+			};
+
+			if (watcher.Path == "Setting not found")
+			{
+				AppSettingsHelper.AddUpdateSetting("Path", AppDomain.CurrentDomain.BaseDirectory);
+			}
+
+
+			if (watcher.Filter == "Setting not found")
+			{
+				AppSettingsHelper.AddUpdateSetting("Filter", "*.*");
+			}
+
+			watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+			                       | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+			// Event handlers
+			watcher.Changed += OnChanged;
+			watcher.Created += OnCreated;
+			watcher.Deleted += OnDeleted;
+			watcher.Renamed += OnRenamed;
+
+			// Begin watching
+			watcher.EnableRaisingEvents = true;
+		}
+
+		private static void OnRenamed(object sender, RenamedEventArgs renamedEventArgs)
+		{
+			Log.Warn(
+				$"{DateTime.Now:MMM ddd d HH:mm yyyy} Zmieniono nazwę pliku z {renamedEventArgs.OldFullPath} na {renamedEventArgs.FullPath}");
+		}
+
+		private static void OnDeleted(object sender, FileSystemEventArgs fileSystemEventArgs)
+		{
+			Log.Error($"{DateTime.Now:MMM ddd d HH:mm yyyy} Usunięto plik: {fileSystemEventArgs.FullPath}");
+		}
+
+		private static void OnCreated(object sender, FileSystemEventArgs fileSystemEventArgs)
+		{
+			Log.Warn($"{DateTime.Now:MMM ddd d HH:mm yyyy} Utworzono plik: {fileSystemEventArgs.FullPath}");
+		}
+
+		private static void OnChanged(object sender, FileSystemEventArgs fileSystemEventArgs)
+		{
+			Log.Warn($"{DateTime.Now:MMM ddd d HH:mm yyyy} Zmeiniono plik!");
+		}
+	}
+}
--- DirectoryMonitor/Services/WatcherService.cs
+++ DirectoryMonitor/Services/WatcherService.cs
@@ -2,45 +2,34 @@
 using System.IO;
 using System.ServiceProcess;
 using System.Timers;
+using DirectoryMonitor.Libs;
 
 namespace DirectoryMonitor.Services
 {
-    partial class WatcherService : ServiceBase
-    {
-//        private FileSystemWatcher _watcher;
-        private EventLog _eventLog;
-        private int eventId = 1;
+	partial class WatcherService : ServiceBase
+	{
+		private Timer _timer = null;
 
-        public WatcherService()
-        {
-            InitializeComponent();
-//            _watcher = new FileSystemWatcher();
-            _eventLog = new EventLog();
+		public WatcherService()
+		{
+			InitializeComponent();
+		}
 
-            if (!EventLog.SourceExists("WatcherSource"))
-            {
-                EventLog.CreateEventSource("WatcherSource", "WatcherLog");
-            }
+		protected override void OnStart(string[] args)
+		{
+			_timer = new Timer {Interval = 1000};
+			_timer.Elapsed += OnTimer;
+			_timer.Enabled = true;
+			WatcherLib.StartMonitoring();
+		}
 
-            _eventLog.Source = "WatcherSource";
-            _eventLog.Log = "WatcherLog";
-        }
+		public void OnTimer(object sender, ElapsedEventArgs args)
+		{
+		}
 
-        protected override void OnStart(string[] args)
-        {
-            // TODO: Add code here to start your service.
-            _eventLog.WriteEntry("Hello from OnStart");
-            var timer = new Timer {Interval = 10000};
-            timer.Elapsed += OnTimer;
-        }
-
-        public void OnTimer(object sender, ElapsedEventArgs args)
-        {
-            _eventLog.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
-        }
-        protected override void OnStop()
-        {
-            // TODO: Add code here to perform any tear-down necessary to stop your service.
-        }
-    }
+		protected override void OnStop()
+		{
+			WatcherLib.StopMonitoring();
+		}
+	}
 }
